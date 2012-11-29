ff16d59ee4f701842f980324013a1b105e11f986
Matt Bailey <mbailey@system42.net>
2012-11-29 02:12:55 -0800

    working ssh and chefrun

--- lib/gaptool-client.rb
+++ lib/gaptool-client.rb
@@ -28,27 +28,78 @@ class SshCommand < Clamp::Command
   option ["-r", "--role"], "ROLE", "Role name to ssh to", :required => true
   option ["-e", "--environment"], "ENVIRONMENT", "Which environment, e.g. production", :required => true
   option ["-i", "--instance"], "INSTANCE", "Node instance, leave blank to query avilable nodes", :require => false
+  option ["-f", "--first"], :flag, "Just connect to first available instance", :require => false
 
   def execute
     if instance
       @ssh = $api.ssh(role, environment, instance)
-      File.open('/tmp/gtkey', 'w') {|f| f.write(@ssh['key'])}
-      File.chmod(0600, '/tmp/gtkey')
-      system "SSH_AUTH_SOCK='' ssh -i /tmp/gtkey admin@#{@ssh['hostname']}"
     else
-      puts "No node number selected; querying provider"
-      gethosts(resource, environment).each do |host|
-        puts host
+      nodes = $api.getenvroles(role, environment)
+      if first? || nodes.size == 1
+        puts "No instnace specified, but only one instance in cluster or first forced"
+        @ssh = $api.ssh(role, environment, nodes.first['instance'])
+      else
+        puts "No instance specified, querying list."
+        nodes.each_index do |i|
+          puts "#{i}: #{nodes[i]['instance']}"
+        end
+        print "Select a node: ".color(:cyan)
+        @ssh = $api.ssh(role, environment, nodes[$stdin.gets.chomp.to_i]['instance'])
       end
-      puts "Select number (just the number):"
-      number = gets
-      `ssh admin@#{resource}-#{environment}-#{number}.#{DOMAIN}`
+    end
+    File.open('/tmp/gtkey', 'w') {|f| f.write(@ssh['key'])}
+    File.chmod(0600, '/tmp/gtkey')
+    system "SSH_AUTH_SOCK='' ssh -i /tmp/gtkey admin@#{@ssh['hostname']}"
+  end
+
+end
+
+def sshcmd(node, commands)
+  Net::SSH.start(
+    node['hostname'],
+    'admin',
+    :key_data => [$api.ssh(node['role'], node['environment'], node['instance'])['key']],
+    :config => false,
+    :keys_only => true,
+    :paranoid => false
+  ) do |ssh|
+    commands.each do |command|
+      command.color(:cyan)
+      ssh.exec! command do
+        |ch, stream, line|
+        puts "#{node['role'].color(:yellow)}:#{node['environment'].color(:yellow)}:#{node['instance'].color(:yellow)}> #{line}"
+      end
     end
   end
 end
 
-class ChefrunCommand < Clamp::Command
 
+class ChefrunCommand < Clamp::Command
+  option ["-r", "--role"], "ROLE", "Role name to ssh to", :required => true
+  option ["-e", "--environment"], "ENVIRONMENT", "Which environment, e.g. production", :required => true
+
+  def execute
+    nodes = $api.getenvroles(role, environment)
+    nodes.peach do |node|
+      json = {
+        'this_server' => "#{role}-#{environment}-#{node['instance']}",
+        'role' => role,
+        'app_user' => node['appuser'],
+        'run_list' => [ "recipe[main]" ],
+        'hostname' => node['hostname'],
+        'instance' => node['instance'],
+        'zone' => node['zone'],
+        'itype' => node['itype'],
+        'apps' => node['apps']
+      }.to_json
+      commands = [
+        "cd ~admin/ops; git pull",
+        "echo '#{json}' > ~admin/solo.json",
+        "sudo chef-solo -c ~admin/ops/cookbooks/solo.rb -j ~admin/solo.json"
+      ]
+      sshcmd(node, commands)
+    end
+  end
 end
 
 class DeployCommand < Clamp::Command
